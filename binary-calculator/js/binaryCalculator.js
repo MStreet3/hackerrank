75f2daee0b47cae03696576bf68cb85830688130
Michael Street <mi.street@gmail.com>
2019-04-24 13:02:48 -0400

    update styles

--- binary-calculator/js/binaryCalculator.js
+++ binary-calculator/js/binaryCalculator.js
@@ -1,15 +1,16 @@
-let btnIds = [['res',''], ['btn0', '0'], ['btn1', '1'], ['btnClr', 'C'],['btnEql', '='],['btnSum', '+'],['btnSub','-'],['btnMul','*'],['btnDiv','/']];
+let btnIds = [['btn0', '0'], ['btn1', '1'], ['btnClr', 'C'],['btnEql', '='],['btnSum', '+'],['btnSub','-'],['btnMul','*'],['btnDiv','/']];
 
 let container = document.getElementById('btns');
 
 // add the buttons to the container
 let btns = btnIds.map(btn => {
-    let id, innerHtml = btn;
+    let [id, innerHtml] = btn;
     let btnElem = document.createElement('button');
     btnElem.id = id;
+    btnElem.className = "btns";
     btnElem.innerHTML = innerHtml;
-    if(['=', '+', '-', '*'].includes(innerHtml)){
-        btnElem.className = 'operator';
+    if(['=', '+', '-', '*', '/'].includes(innerHtml)){
+        btnElem.className += ' operator';
     }
     container.appendChild(btnElem);
     return btnElem;
